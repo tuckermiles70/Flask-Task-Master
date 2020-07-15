4fdf42620e28c75fc24a399dc79753efbb3cdbc4
Tucker <tmiles7@vols.utk.edu>
2020-07-15 16:29:49 -0400

    Update logic added

--- app.py
+++ app.py
@@ -40,11 +40,25 @@ def delete(id):
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
-        redirect('/')
+        return redirect('/')
     except:
         return 'There was a problem deleting that task'
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+    else:
+        return render_template('update.html', task=task)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
